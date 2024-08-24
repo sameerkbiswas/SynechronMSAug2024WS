999c74296171ade02a935d452aceb485fafaebdb
Shalini Mittal <shalini06.mittal@gmail.com>
2024-08-24 14:49:07 +0530

    updated for links

--- SpringBootRestDemo/src/main/java/com/web/SpringBootRestDemo/SpringBootRestDemoApplication.java
+++ SpringBootRestDemo/src/main/java/com/web/SpringBootRestDemo/SpringBootRestDemoApplication.java
@@ -3,8 +3,6 @@ package com.web.SpringBootRestDemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//https://vladmihalcea.com/the-best-way-to-map-a-composite-primary-key-with-jpa-and-hibernate/
-//https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#jpa.query-methods
 @SpringBootApplication
 public class SpringBootRestDemoApplication {
 
